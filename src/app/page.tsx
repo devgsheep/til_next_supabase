8ea07e5aadf2bc0fab3317867631a8da115ddf7d
dev.gsheep <dev.gsheep@gmail.com>
2025-10-20 10:58:55 +0900

    [docs] scss 설정 및 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import ButtonTest from '@/components/ButtonTest';
+import SCSSTest from '@/components/SCSSTest';
 import React from 'react';
 
 function page() {
   return (
     <div>
       <ButtonTest />
+      <SCSSTest />
     </div>
   );
 }
 
 export default page;
-
