b3587e7358a0c153680aeec1dc1f0738f3f3ff71
dev.gsheep <dev.gsheep@gmail.com>
2025-10-21 13:44:15 +0900

    [docs] react query 설정 및 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,7 @@
-import ButtonTest from '@/components/ButtonTest';
-import Counter from '@/components/Counter';
-import SCSSTest from '@/components/SCSSTest';
-import ThemeToggle from '@/components/ThemeToggle';
-import TodoList from '@/components/TodoList';
-import UserProfile from '@/components/UserProfile';
-import React from 'react';
-
 function page() {
   return (
     <div>
-      <ButtonTest />
-      <SCSSTest />
-      <Counter />
-      <br />
-      <br />
-      <UserProfile />
-      <br />
-      <ThemeToggle />
-      <br />
-      <br />
-      <TodoList />
+      <h2>React Query</h2>
     </div>
   );
 }
